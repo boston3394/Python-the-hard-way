4f6022786b657f18bdae05291b2adfce0f339072
boston3394 <boston3394@users.noreply.github.com>
2017-07-13 17:13:37 -0400

    Add files via upload

---
+++ python_notes.py
@@ -0,0 +1,20 @@
+PYTHON NOTES
+
+Escape	What it does.
+\\	Backslash (\)
+\'	Single-quote (')
+\"	Double-quote (")
+\a	ASCII bell (BEL)
+\b	ASCII backspace (BS)
+\f	ASCII formfeed (FF)
+\n	ASCII linefeed (LF)
+\N{name}	Character named name in the Unicode database (Unicode only)
+\r	Carriage Return (CR)
+\t	Horizontal Tab (TAB)
+\uxxxx	Character with 16-bit hex value xxxx (u'' string only)
+\Uxxxxxxxx	Character with 32-bit hex value xxxxxxxx (u'' string only)
+\v	ASCII vertical tab (VT)
+\ooo	Character with octal value ooo
+\xhh	Character with hex value hh
+
+https://first-web-scraper.readthedocs.io/en/latest/ - useful python tutorial
