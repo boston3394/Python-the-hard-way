3299375baf4d228be6bc2f0e51e41bf7b434082f
boston3394 <boston3394@users.noreply.github.com>
2017-07-14 08:37:39 -0400

    Add files via upload

--- python_notes.py
+++ python_notes.py
@@ -1,5 +1,7 @@
 PYTHON NOTES
 
+Use ATOM as your text editor
+
 Escape	What it does.
 \\	Backslash (\)
 \'	Single-quote (')
@@ -18,3 +20,27 @@ Escape	What it does.
 \xhh	Character with hex value hh
 
 https://first-web-scraper.readthedocs.io/en/latest/ - useful python tutorial
+
+django-admin startproject firstdjango
+
+get-pip.py 
+
+python manage.py to see list of available commands
+
+to run them its python manage.py <command name>
+
+python manage.py runserver
+
+localhost:8000
+
+python manage.py startapp to start your app 
+
+role of folders
+manage.py runs commands
+init_.py tells django where project folder is
+wsgi.py provides a hook for webservers
+settings.pyconfigures django - DO EDIT
+urls.py routes requests on url - DO EDIT
+
+Django - an app is a folder with python files. it is a componant
+program is the overall thing that contains multiple apps
